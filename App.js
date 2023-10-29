0d01bd2415737cf23c2e042d832822a3e3338b6c
bharat khairnar <bharatdkhairnar@gmail.com>
2023-10-29 11:58:42 +0530

    Add header logo and user icon

--- App.js
+++ App.js
@@ -1,6 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1",{'id':"heading"} , "Hello world from React!");
+// React Element
+const heading = (<h1 id="heading" className="heading" >
+    I learn Namaste React
+    </h1>);
+
+// React Component
+
+const HeaderLogo = () => (
+    <div className="logo">
+        <img src="/images/logo.png"></img>
+    </div>
+)
+
+const UserIcon = () => (
+    <div className="user-icon">
+        <img src="/images/user-icon.png" />
+    </div>
+)
+
+const HeaderSearchBar = () => (
+    <div className="searchbar">
+        <label className="searchbar"></label>
+        <input type="text"></input>
+    </div>
+)
+
+const HeadingComponent = () => (
+    <div className="header">
+        <HeaderLogo />
+        <HeaderSearchBar />
+        <UserIcon />
+    </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent />);
